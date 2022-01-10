2c5a1591644c4095a59bd96b298b331b8ea4c7c5
son-yu-seoung <thsdbtjd10@naver.com>
2022-01-10 23:41:40 +0900

    help me.. - yu

--- App/app/src/main/java/com/example/app/MainActivity.java
+++ App/app/src/main/java/com/example/app/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Adapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    Adapter adapter;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.view);
+        adapter = new Adapter(this);
+        viewPager.setAdapter(adapter);
     }
 }
