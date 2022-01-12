f53e35dd3735be26d83def56f17e25eb9c1ff76e
최승식(ChoiSeungSik) <tmdtlr0911@naver.com>
2022-01-12 21:58:45 +0900

    E_Info2csv The final version

--- AI/scripts/E_Info2csv.py
+++ AI/scripts/E_Info2csv.py
@@ -48,11 +48,63 @@ while True:
 
     elif select == 2: # data retouch
         print('\n==================== 데이터 수정 ====================\n')
-        pass
-
+        target_name = str(input('수정할 사람의 이름을 입력해주세요'))
+        target_location = excersize_df.index[excersize_df['Name'] == target_name]
+        target_location = target_location.tolist()
+        target_location = target_location[0]
+        
+        while True:
+            print('\n============== 수정할 부위을 입력해주세요 ==============\n')
+            body_select = int(input('0. Arm, 1. Leg, 2. Neck, 3. Finger, 4. Foot, 5. Eyes, 6. Ears, 7. Noise, 8. 종료'))
+            
+            if body_select == 0:
+                change_value = int(input())
+                excersize_df.at[target_location, 'Arm'] = change_value
+            
+            elif body_select == 1:
+                change_value = int(input())
+                excersize_df.at[target_location, 'Leg'] = change_value
+            
+            elif body_select == 2:
+                change_value = int(input())
+                excersize_df.at[target_location, 'Neck'] = change_value
+            
+            elif body_select == 3:
+                change_value = int(input())
+                excersize_df.at[target_location, 'Finger'] = change_value
+            
+            elif body_select == 4:
+                change_value = int(input())
+                excersize_df.at[target_location, 'Foot'] = change_value
+            
+            elif body_select == 5:
+                change_value = int(input())
+                excersize_df.at[target_location, 'Eyes'] = change_value
+            
+            elif body_select == 6:
+                change_value = int(input())
+                excersize_df.at[target_location, 'Ears'] = change_value
+            
+            elif body_select == 7:
+                change_value = int(input())
+                excersize_df.at[target_location, 'Noise'] = change_value
+            
+            elif body_select == 8:
+                excersize_df.to_csv('./excersize_info.csv')
+                print('종료하겠습니다.\n')
+                time.sleep(2)
+                break
+        
     elif select == 3: # data delete
         print('\n==================== 데이터 삭제====================\n')
-        pass
+        target_name = str(input('수정할 사람의 이름을 입력해주세요.\n'))
+        target_location = excersize_df.index[excersize_df['Name'] == target_name]
+        target_location = target_location.tolist()
+        target_location = target_location[0]
+        
+        excersize_df = excersize_df.drop([target_location])
+        print(excersize_df)
+        excersize_df.to_csv('./excersize_info.csv')
 
     elif select == 4: # exit the program
         print('\n==================== 프로그램 종료 ====================\n')
