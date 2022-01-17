46f74b4e3b8258e451c50723b7083ac1c35454fa
JunPyo <vy4515@naver.com>
2022-01-17 17:32:01 +0900

    Merge remote-tracking branch 'origin/yuseong' into junpyo
    # Conflicts:
    #	App/app/src/main/res/layout/activity_main.xml

---
+++ App/app/src/main/java/com/example/app/login_page.java
@@ -0,0 +1,21 @@
+package com.example.app;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login_page extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // -> 가로로 화면 전환
+        setContentView(R.layout.login_page);
+
+
+    }
+}
