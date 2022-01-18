24b065a78b07587e3bd8bd02a93eb3b8c97b761e
JunPyo <vy4515@naver.com>
2022-01-18 15:22:09 +0900

    Merge remote-tracking branch 'origin/yuseong' into junpyo
    # Conflicts:
    #	App/app/src/main/res/layout/activity_main.xml

--- App/app/src/main/java/com/example/app/MainActivity.java
+++ App/app/src/main/java/com/example/app/MainActivity.java
@@ -2,31 +2,23 @@ package com.example.app;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // PrograssBar Start
-    int pStatus = 0;
-    private Handler handler = new Handler();
-    TextView tv;
-    // PrograssBar End
+
 
     // Spinner Start
     TextView textView;
@@ -38,43 +30,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // -> 가로로 화면 전환
         setContentView(R.layout.activity_main);
-
-
-        // ProgressBar Code Start
-        Resources res = getResources();
-        Drawable drawable = res.getDrawable(R.drawable.circular);
-        final ProgressBar mProgress = (ProgressBar) findViewById(R.id.circularProgressbar);
-        mProgress.setProgress(0);   // Main Progress
-        mProgress.setSecondaryProgress(100); // Secondary Progress
-        mProgress.setMax(100); // Maximum Progress
-        mProgress.setProgressDrawable(drawable);
-
-        tv = (TextView) findViewById(R.id.tv);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // TODO Auto-generated method stub
-                while (pStatus < 100) {
-                    pStatus += 1;
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            // TODO Auto-generated method stub
-                            mProgress.setProgress(pStatus);
-                            tv.setText(pStatus + "%");
-                        }
-                    });
-                    try {
-                        // Sleep for 200 milliseconds.
-                        // Just to display the progress slowly
-                        Thread.sleep(8); //thread will take approx 1.5 seconds to finish
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
-        // PrograssBar End
 
         // Button 전환
 
@@ -123,5 +78,6 @@ public class MainActivity extends AppCompatActivity {
 
         */
     }
-
+    
 }
+
